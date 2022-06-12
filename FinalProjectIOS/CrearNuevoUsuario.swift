d5a922e2a3ecae3cb5ec02c92242a29cfd02c04c
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-06-11 22:06:03 -0700

    added stuff

---
+++ FinalProjectIOS/CrearNuevoUsuario.swift
@@ -0,0 +1,21 @@
+//
+//  CrearNuevoUsuario.swift
+//  FinalProjectIOS
+//
+//  Created by Estefania Jimenez Garcia  on 11/06/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct CrearNuevoUsuario: View {
+    var body: some View {
+        Text("Crear Nuevo Usuario")
+    }
+}
+
+struct CrearNuevoUsuario_Previews: PreviewProvider {
+    static var previews: some View {
+        CrearNuevoUsuario()
+    }
+}
