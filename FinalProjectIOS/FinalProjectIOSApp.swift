a724c7b3af31b3a34e15c2d0f70728d23f1344a2
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-06-11 20:35:21 -0700

    Initial Commit

---
+++ FinalProjectIOS/FinalProjectIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectIOSApp.swift
+//  FinalProjectIOS
+//
+//  Created by Estefania Jimenez Garcia  on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
